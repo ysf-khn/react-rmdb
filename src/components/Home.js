d5080189aed64ba5488d7ed8b580c492fcf06b3a
Yusuf <yusufmohd72@gmail.com>
2021-10-19 20:28:00 +0530

    Fetching movie data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,9 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+//API
+import API from '../API';
 
 //Config
-
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 
 //Components
@@ -14,7 +16,24 @@ import NoImage from '../images/no_image.jpg';
 const Home = () => {
   const [state, setState] = useState();
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState();
+  const [error, setError] = useState(false);
+
+  const fetchMovies = async (page, searchTerm = '') => {
+    try {
+      setError(false);
+      setLoading(true);
+
+      const movies = await API.fetchMovies(page, searchTerm);
+      console.log(movies);
+    } catch (error) {
+      setError(true);
+    }
+  };
+
+  //Initial render
+  useEffect(() => {
+    fetchMovies(1);
+  }, []);
 
   return <div>Homepage</div>;
 };
