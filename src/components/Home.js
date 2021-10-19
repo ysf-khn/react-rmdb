96b8324844427e3f9434e757a42f2cae241a0b25
Yusuf <yusufmohd72@gmail.com>
2021-10-19 23:31:22 +0530

    custom hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,39 +1,17 @@
-import { useState, useEffect } from 'react';
-
-//API
-import API from '../API';
-
 //Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 
 //Components
 
 //Hook
+import useHomeFetch from '../hooks/useHomeFetch';
 
 //Image
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-  const [state, setState] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const fetchMovies = async (page, searchTerm = '') => {
-    try {
-      setError(false);
-      setLoading(true);
-
-      const movies = await API.fetchMovies(page, searchTerm);
-      console.log(movies);
-    } catch (error) {
-      setError(true);
-    }
-  };
-
-  //Initial render
-  useEffect(() => {
-    fetchMovies(1);
-  }, []);
+  const { state, loading, error } = useHomeFetch();
+  console.log(state);
 
   return <div>Homepage</div>;
 };
---
+++ src/hooks/useHomeFetch.js
@@ -0,0 +1,44 @@
+import { useState, useEffect, useRef } from 'react';
+
+import API from '../API';
+
+const initialState = {
+  page: 0,
+  results: [],
+  total_pages: 0,
+  total_results: 0,
+};
+
+const useHomeFetch = () => {
+  const [state, setState] = useState(initialState);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const fetchMovies = async (page, searchTerm = '') => {
+    try {
+      setError(false);
+      setLoading(true);
+
+      const movies = await API.fetchMovies(page, searchTerm);
+
+      setState(prev => ({
+        ...movies,
+        results:
+          page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
+      }));
+    } catch (error) {
+      setError(true);
+    }
+
+    setLoading(false);
+  };
+
+  //Initial render
+  useEffect(() => {
+    fetchMovies(1);
+  }, []);
+
+  return { state, loading, error };
+};
+
+export default useHomeFetch;
