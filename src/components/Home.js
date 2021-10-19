43f7948251977e7e25efa9f81971dc7663d6ada6
Yusuf <yusufmohd72@gmail.com>
2021-10-19 18:49:20 +0530

    HomePage component init

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+//Config
+
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
+
+//Components
+
+//Hook
+
+//Image
+import NoImage from '../images/no_image.jpg';
+
+const Home = () => {
+  const [state, setState] = useState();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+
+  return <div>Homepage</div>;
+};
+
+export default Home;
