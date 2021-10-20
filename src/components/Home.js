4c9f5fed7ca70d6de6fc15b2c668fb36b6907377
Yusuf <yusufmohd72@gmail.com>
2021-10-20 19:43:24 +0530

    Hero section

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 
 //Components
+import HeroImage from './HeroImage';
 
 //Hook
 import useHomeFetch from '../hooks/useHomeFetch';
@@ -13,7 +14,17 @@ const Home = () => {
   const { state, loading, error } = useHomeFetch();
   console.log(state);
 
-  return <div>Homepage</div>;
+  return (
+    <>
+      {state.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+    </>
+  );
 };
 
 export default Home;
